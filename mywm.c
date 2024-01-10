6afe470ed97a115fd6c94595a52641a64056397b
Leonardo Estacio <mdnssknght@gmail.com>
2024-01-10 12:19:23 -0500

    mywm: keybinding support
    Signed-off-by: Leonardo Estacio <mdnssknght@gmail.com>

--- mywm.c
+++ mywm.c
@@ -190,24 +190,53 @@ void keyboard_modifiers(struct wl_listener *listener, void *data) {
 			&keyboard->wlr_keyboard->modifiers);
 }
 
+bool keyboard_keybinding(struct mywm_server *server, xkb_keysym_t sym) {
+	switch(sym) {
+	case XKB_KEY_Escape:
+		wl_display_terminate(server->wl_display);
+		break;
+	default:
+		return false;
+	}
+	return true;
+}
+
 void keyboard_key(struct wl_listener *listener, void *data) {
 	struct wlr_seat *seat;
 	struct wlr_keyboard_key_event *event;
+	const xkb_keysym_t *syms;
 	struct mywm_keyboard *keyboard;
+	unsigned int keycode;
+	unsigned int modifiers;
+	bool handled = false;
+	int nsyms;
 
 	event = data;
 	keyboard = wl_container_of(listener, keyboard, key);
 	seat = keyboard->server->seat;
 
-	wlr_seat_set_keyboard(seat, keyboard->wlr_keyboard);
-	wlr_seat_keyboard_notify_key(seat, event->time_msec,
-			event->keycode, event->state);
+	keycode = event->keycode + 8;
+	nsyms = xkb_state_key_get_syms(keyboard->wlr_keyboard->xkb_state,
+			keycode, &syms);
+	modifiers = wlr_keyboard_get_modifiers(keyboard->wlr_keyboard);
+	if ((modifiers & WLR_MODIFIER_LOGO) &&
+			event->state == WL_KEYBOARD_KEY_STATE_PRESSED) {
+		for (int i = 0; i < nsyms; i++) {
+			handled = keyboard_keybinding(keyboard->server, syms[i]);
+		}
+	}
+
+	if (!handled) {
+		wlr_seat_set_keyboard(seat, keyboard->wlr_keyboard);
+		wlr_seat_keyboard_notify_key(seat, event->time_msec,
+				event->keycode, event->state);
+	}
 }
 
 void keyboard_destroy(struct wl_listener *listener, void *data) {
 	struct mywm_keyboard *keyboard;
 
-	keyboard = wl_container_of(listener, keyboard, key);
+	keyboard = wl_container_of(listener, keyboard, destroy);
 
 	wl_list_remove(&keyboard->modifiers.link);
 	wl_list_remove(&keyboard->key.link);
